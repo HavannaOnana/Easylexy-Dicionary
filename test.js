9d5758430064b5e36c0ca5245a434e7f1486ae4d
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-08-09 18:02:33 +0200

    added python file to test skill

--- test.js
+++ test.js
@@ -3,15 +3,16 @@ for(let i = 1; i<=10; i++){
     Numbers.push(i)
 }
 
-const result = document.querySelector("#result");
-const search = document.querySelector("#search").value;
+const result = document.querySelector(".result");
+let search = document.querySelector("#search").value;
+search = parseInt(search);
 const button = document.querySelector("#button")
 
 button.addEventListener("click",function(){
-    if(search === 0){
-        console.log("No number")
-    }
-    else{
-        console.log(search)
-    }
+   if(Numbers.includes(search)){
+    result.innerHTML = `<h1>${search}</h1>`
+   }
+   else{
+    result.innerHTML = `<h1>Number not in the list</h1>`
+   }
 })
